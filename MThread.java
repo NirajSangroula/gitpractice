9b025cad0c7200dbd9014d12c7f7d8c747eb1dff
Niraj Sangroula <sangroulaniraj15@gmail.com>
2021-05-10 18:49:47 +0545

    Added .gitignore and MThread changed

--- MThread.java
+++ MThread.java
@@ -2,7 +2,7 @@ import java.io.*;
 public class MThread{
     public static void main(String args[]){
         System.out.print("Hello");
-        System.out.println("amazing");
         System.out.println("Just");
+	System.out.print("New statement");
     }
 }
