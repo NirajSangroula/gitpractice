f28d983e4bf9c2f8feec90c246227217b54e34f8
Niraj Sangroula <sangroulaniraj15@gmail.com>
2021-05-10 18:33:56 +0545

    Initial commit

---
+++ MThread.java
@@ -0,0 +1,8 @@
+import java.io.*;
+public class MThread{
+    public static void main(String args[]){
+        System.out.print("Hello");
+        System.out.println("amazing");
+        System.out.println("Just");
+    }
+}
