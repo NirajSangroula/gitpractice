f9876af5ffc744bd4c914f1623430faee08e3e41
Niraj Sangroula <sangroulaniraj15@gmail.com>
2021-05-13 10:56:33 +0545

    finish up

--- MThread.java
+++ MThread.java
@@ -3,7 +3,11 @@ public class MThread{
     public static void main(String args[]){
         System.out.print("Hello");
         System.out.println("Just");
+<<<<<<< HEAD
 	"Later in main"
 	System.out.print("New statement");
+=======
+	System.out.print("Let me add or, change over this things");
+>>>>>>> printLove
     }
 }
