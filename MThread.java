f298ec037721f98eb3ea57b9aa057573a0f9d2d7
Niraj Sangroula <sangroulaniraj15@gmail.com>
2021-05-13 08:53:18 +0545

    CHanges in Mthread

--- MThread.java
+++ MThread.java
@@ -3,6 +3,7 @@ public class MThread{
     public static void main(String args[]){
         System.out.print("Hello");
         System.out.println("Just");
+	"Later in main"
 	System.out.print("New statement");
     }
 }
