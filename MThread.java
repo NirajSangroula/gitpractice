4824ff48dae6db141c920b97f9c8b87221fd89cf
Niraj Sangroula <sangroulaniraj15@gmail.com>
2021-05-13 08:56:10 +0545

    Changed in branch

--- MThread.java
+++ MThread.java
@@ -3,5 +3,6 @@ public class MThread{
     public static void main(String args[]){
         System.out.print("Hello");
         System.out.println("Just");
+	System.out.print("Let me add or, change over this things");
     }
 }
